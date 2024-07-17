4ce7b68d47012fc16cbc09f6e34f54ef5e472fa5
spergcity <60526075+spergcity@users.noreply.github.com>
2024-07-17 12:10:55 -0700

    Add files via upload

---
+++ src/pages/src/pages/nfts.tsx
@@ -0,0 +1,223 @@
+// next and react imports
+import React, { useCallback, useState } from 'react';
+import type { NextPage } from 'next';
+
+import styles from '../styles/Nfts.module.css';
+
+import { Table, TableBody, TableCell, TableContainer, TableRow, Typography, Box } from '@mui/material';
+
+import IconButton from '@mui/material/IconButton';
+import IncrementIcon from '@mui/icons-material/Add'; // Import Add Icon
+import DecrementIcon from '@mui/icons-material/Remove'; // Import Remove Icon
+
+// rainbowkit+ imports
+import {
+    useAccount,
+    useReadContract,
+} from 'wagmi';
+
+// abi objects
+import { nft_abi } from '../../abi_objects/nft_abi';
+import { token_abi } from '../../abi_objects/token_abi';
+import { staking_nft_abi } from '../../abi_objects/staking_nft_abi';
+
+// component imports
+import NftCard from '../../components/cards/NftCard';
+import NftApproveAndActionCard from '../../components/approval_cards/NftApproveAndActionCard';
+
+import { toWei } from 'web3-utils';
+
+const Nfts: NextPage = () => {
+    const [totalMinted, setTotalMinted] = React.useState(0n);
+    const [totalStakedState, setTotalStakedState] = React.useState(0n);
+    const [userStakedState, setUserStakedState] = React.useState(0n);
+    const [mounted, setMounted] = React.useState(false);
+    const [hasRenderedOnce, setHasRenderedOnce] = React.useState(false);
+    const [mintCount, setMintCount] = React.useState(1);
+    const [currentAllowanceState, setStateAllowanceAmount] = React.useState(0n);
+    const [ownedNftCardProps, setOwnedNftCardProps] = React.useState(new Array);
+
+    const { address, isConnected } = useAccount();
+
+    React.useEffect(() => setMounted(true), []);
+
+    // contract config objects
+    const nftContractConfig = {
+        address: process.env.NEXT_PUBLIC_NFT_ADDRESS as '0x${string}',
+        abi: nft_abi
+    } as const;
+
+    const nftOwnerContractConfig = {
+        address: process.env.NEXT_PUBLIC_NFT_ADDRESS as '0x${string}',
+        abi: nft_abi,
+        args: [address as '0x${string}']
+    } as const;
+
+    const allowanceContractConfig = {
+        address: process.env.NEXT_PUBLIC_TOKEN_ADDRESS as '0x${string}',
+        abi: token_abi,
+        args: [address as '0x${string}', process.env.NEXT_PUBLIC_NFT_ADDRESS as '0x${string}']
+    } as const;
+
+    const totalStakedContractConfig = {
+        address: process.env.NEXT_PUBLIC_NFT_STAKING_ADDRESS as '0x${string}',
+        abi: staking_nft_abi,
+        functionName: "stakedCount"
+    } as const;
+
+    const userStakedContractConfig = {
+        address: process.env.NEXT_PUBLIC_NFT_STAKING_ADDRESS as '0x${string}',
+        abi: staking_nft_abi,
+        args: [address as '0x${string}'],
+        functionName: "userInfo"
+    } as const;
+
+    //// READ OPERATIONS
+    const { data: ownedNfts } = useReadContract({
+        ...nftOwnerContractConfig,
+        functionName: 'tokensOfOwner',
+    });
+
+    const { data: totalSupplyData } = useReadContract({
+        ...nftContractConfig,
+        functionName: 'totalSupply',
+    });
+
+    const { data: allowanceAmount } = useReadContract({
+        ...allowanceContractConfig,
+        functionName: "allowance"
+    });
+
+    const { data: totalStaked } = useReadContract({
+        ...totalStakedContractConfig,
+    });
+
+    const { data: userStaked } = useReadContract({
+        ...userStakedContractConfig,
+    });
+
+
+    //// STATE UPDATES
+    // update state with the read results
+    React.useEffect(() => {
+        if (ownedNfts && currentAllowanceState >= 0) {
+
+            const nftCardsData = ownedNfts.map(tokenId => ({
+                mounted: mounted, // Example value, replace with actual logic if needed
+                isConnected: isConnected, // Example value, replace with actual logic if needed
+                tokenId,
+                nftContractConfig: nftContractConfig, // Example value, replace with actual logic if needed
+                currentAllowanceState: currentAllowanceState,
+                setStateAllowanceAmount: setStateAllowanceAmount
+            }));
+
+            setOwnedNftCardProps(nftCardsData)
+        }
+    }, [ownedNfts, currentAllowanceState]);
+
+    console.log("current allowance: ", currentAllowanceState)
+
+    React.useEffect(() => {
+        if (totalSupplyData) {
+            setTotalMinted(totalSupplyData);
+        }
+    }, [totalSupplyData]);
+
+    React.useEffect(() => {
+        if (totalStaked) {
+            setTotalStakedState(totalStaked);
+        }
+    }, [totalStaked]);
+
+    React.useEffect(() => {
+        if (userStaked) {
+            setUserStakedState(userStaked[0]);
+        }
+    }, [userStaked]);
+
+    // update state with the read results
+    React.useEffect(() => {
+        if (allowanceAmount && !hasRenderedOnce) {
+            setStateAllowanceAmount(allowanceAmount);
+            setHasRenderedOnce(true);
+            console.log("parent action")
+        }
+    }, [allowanceAmount, hasRenderedOnce]);
+
+    // state management for user to select number of NFTs to mint
+    const incrementCount = () => {
+        setMintCount(mintCount + 1);
+    };
+
+    const decrementCount = () => {
+        if (mintCount > 1) {
+            setMintCount(mintCount - 1);
+        }
+    };
+
+    const tokenId = 0
+
+    // TODO - make NFTS populate iteratively and fix the always minting forever bug
+    // TODO - make unstake flip the card and have the unstake button on the back
+
+    return (
+        <div className="page" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '30px' }}>
+           <div className="nfttop">
+            <Typography className="container" variant="h2">NFT Zone</Typography>
+            <div className='container'>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className={styles.table}>NFTs in circulation:</TableCell>
+                                <TableCell className={styles.table}>{Number(totalMinted)}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={styles.table}>Total level 5NFTs staked:</TableCell>
+                                <TableCell className={styles.table}>{Number(totalStakedState)}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell className={styles.table}>Level 5 NFTs staked by 0x{String(address).slice(-4)}:</TableCell>
+                                <TableCell className={styles.table}>{Number(userStakedState)}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            <div className='container'>
+                <Typography variant="h3" sx={{ marginTop: "12px" }}>Approve and Mint</Typography>
+
+            </div>
+            <div className="container" style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
+                <Typography>Number of NFTS to mint and/or level up: </Typography>
+                <Box sx={{ border: '1px solid', borderColor: 'secondary.main', borderRadius: '4px', p: 1 }}>
+                    <IconButton onClick={decrementCount}>
+                        <DecrementIcon />
+                    </IconButton>
+                    <span>{mintCount}</span>
+                    <IconButton onClick={incrementCount}>
+                        <IncrementIcon />
+                    </IconButton>
+                </Box>
+            </div>
+            </div>
+            <NftApproveAndActionCard mounted={mounted} isConnected={isConnected} cardTitle={"Approve burning $PROPHET"} amountToApprove={(Number(toWei(mintCount, "ether")) * 400000.01)} allowanceAmount={Number(currentAllowanceState)} mintCount={mintCount} totalMinted={totalMinted} setTotalMinted={setTotalMinted} setOwnedNftCardProps={setOwnedNftCardProps} setStateAllowanceAmount={setStateAllowanceAmount}></NftApproveAndActionCard>
+            <Typography className="container" variant="h3">NFTs at 0x{String(address).slice(-4)}</Typography>
+            {ownedNftCardProps.map((cardData, index) => (
+                <NftCard
+                    key={index}
+                    mounted={cardData.mounted}
+                    isConnected={cardData.isConnected}
+                    tokenId={cardData.tokenId}
+                    currentAllowanceState={cardData.currentAllowanceState}
+                    setStateAllowanceAmount={cardData.setStateAllowanceAmount}
+                />
+            ))}
+        </div>
+    );
+};
+
+// TODO - make it so staked NFTs use the flip card with unstake on it
+// TODO - disable the stake button as well!
+
+export default Nfts;
---
+++ src/pages/src/pages/prophet.tsx
@@ -0,0 +1,408 @@
+// next and react imports
+import React, { ChangeEvent } from 'react';
+import type { NextPage } from 'next';
+
+import styles from '../styles/Prophet.module.css';
+
+import { TableHead, CircularProgress, Table, TableBody, TableCell, TableContainer, TableRow, Typography, Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+import { token_abi } from '../../abi_objects/token_abi';
+import { staking_token_abi } from '../../abi_objects/staking_token_abi';
+import { ido_vault_abi } from '../../abi_objects/ido_abi';
+
+import { toWei } from 'web3-utils';
+
+// rainbowkit+ imports
+import {
+  useAccount,
+  useReadContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+  useBalance
+} from 'wagmi';
+
+import ProphetApproveAndStakeCard from '../../components/approval_cards/ProphetApproveAndStake';
+import ProphetApproveAndUnstakeCard from '../../components/approval_cards/ProphetApproveAndUnstakeCard';
+
+const Prophet: NextPage = () => {
+  const [mounted, setMounted] = React.useState(false);
+  const [currentTokenBalanceState, setCurrentTokenBalanceState] = React.useState(0n);
+  const [currentETHBalanceState, setCurrentETHBalanceState] = React.useState(0n);
+  const [ethToUseInBuyWei, setEthToUseInBuyWei] = React.useState(0n);
+  const [ethToUseInBuyFloat, setEthToUseInBuyFloat] = React.useState(0);
+  const [tokensThatCanBeBoughtWithCurrentEthState, setTokensThatCanBeBoughtWithCurrentEthState] = React.useState(0n);
+  const [tokensRemaining, setTokensRemaining] = React.useState([0n]);
+  const [userStakedTokensState, setUserStakedTokensState] = React.useState(0n)
+  const [totalStakedTokensState, setTotalStakedTokensState] = React.useState(0n)
+
+  const { address, isConnected } = useAccount();
+
+  const { data: currentEthBalance } = useBalance({
+    address: address,
+    unit: 'ether',
+  });
+
+  React.useEffect(() => setMounted(true), []);
+
+  const balanceOfContractConfig = {
+    address: process.env.NEXT_PUBLIC_TOKEN_ADDRESS as '0x${string}',
+    abi: token_abi,
+    args: [address as '0x${string}'],
+    functionName: 'balanceOf',
+  } as const;
+
+  const buyTokensContractConfig = {
+    address: process.env.NEXT_PUBLIC_IDO_ADDRESS as '0x${string}',
+    abi: ido_vault_abi,
+    args: [BigInt(tokensThatCanBeBoughtWithCurrentEthState)],
+    value: BigInt(ethToUseInBuyWei),
+    functionName: 'buy',
+  } as const;
+
+  const hardcoded_tier = 0
+  const previewPurchaseContractConfig = {
+    address: process.env.NEXT_PUBLIC_IDO_ADDRESS as '0x${string}',
+    abi: ido_vault_abi,
+    args: [ethToUseInBuyWei, BigInt(hardcoded_tier)],
+    functionName: 'previewPurchase',
+  } as const;
+
+  const tokensRemainingContractConfig = {
+    address: process.env.NEXT_PUBLIC_IDO_ADDRESS as '0x${string}',
+    abi: ido_vault_abi,
+    functionName: 'totalSold',
+  } as const;
+
+  const totalTokenStakingContractConfig = {
+    address: process.env.NEXT_PUBLIC_TOKEN_STAKING_ADDRESS as '0x${string}',
+    abi: staking_token_abi,
+    functionName: 'totalStakedAmount'
+  } as const;
+
+  const userTokenStakingContractConfig = {
+    address: process.env.NEXT_PUBLIC_TOKEN_STAKING_ADDRESS as '0x${string}',
+    abi: staking_token_abi,
+    args: [address as '0x${string}'],
+    functionName: 'stakedAmounts'
+  } as const;
+
+  //// READ OPERATIONS
+  const { data: currentBalance } = useReadContract({
+    ...balanceOfContractConfig,
+  });
+
+  const { data: tokensThatCanBeBoughtWithCurrentEth } = useReadContract({
+    ...balanceOfContractConfig,
+  });
+
+  const { data: tokenAmount } = useReadContract({
+    ...previewPurchaseContractConfig,
+  });
+
+  const { data: tokensSold } = useReadContract({
+    ...tokensRemainingContractConfig,
+  });
+
+  const { data: totalStakedProphet } = useReadContract({
+    ...totalTokenStakingContractConfig,
+  });
+
+  const { data: userStakedProphet } = useReadContract({
+    ...userTokenStakingContractConfig,
+  });
+
+  //// WRITE OPERATIONS
+  // let user approve $PROPHET tokens
+  const {
+    data: hash,
+    writeContract: buy,
+    isPending: isBuyLoading,
+    isSuccess: isBuyStarted,
+    error: buyError,
+  } = useWriteContract();
+
+  // approve actions
+  const {
+    data: buyTxData,
+    isSuccess: buyTxSuccess,
+    error: buyTxError,
+  } = useWaitForTransactionReceipt({
+    hash,
+    query: {
+      enabled: !!hash,
+    },
+  });
+
+  React.useEffect(() => {
+    if (currentBalance) {
+      setCurrentTokenBalanceState(currentBalance);
+    }
+  }, [currentBalance]);
+
+  React.useEffect(() => {
+    if (userStakedProphet) {
+      setUserStakedTokensState(userStakedProphet[0]);
+    }
+  }, [userStakedProphet]);
+
+  React.useEffect(() => {
+    if (totalStakedProphet) {
+      setTotalStakedTokensState(totalStakedProphet);
+    }
+  }, [totalStakedProphet]);
+
+  React.useEffect(() => {
+    if (tokenAmount) {
+      setTokensThatCanBeBoughtWithCurrentEthState(tokenAmount);
+    }
+  }, [tokenAmount]);
+
+  React.useEffect(() => {
+    if (!isBuyLoading && isBuyStarted) {
+      setCurrentTokenBalanceState(prevBalance => prevBalance + tokensThatCanBeBoughtWithCurrentEthState);
+
+    }
+  }, [isBuyLoading, isBuyStarted]);
+
+  React.useEffect(() => {
+    if (tokensSold) {
+      let tokensLeftInVault = BigInt(120000000000) - (tokensSold / BigInt(1000000000000000000));
+
+      const maxTokensPerVault = 20000000000
+      let vaultIndex = 0;
+      let vaults: bigint[] = [];
+
+      while (tokensLeftInVault > 0) {
+        let tokensToAdd = BigInt(Math.min(maxTokensPerVault, Number(tokensLeftInVault)));
+
+        vaults[vaultIndex] = tokensToAdd;
+        tokensLeftInVault -= tokensToAdd;
+        vaultIndex++;
+      }
+
+      while (vaults.length < 6) {
+        vaults.push(0n)
+      }
+
+      setTokensRemaining(vaults);
+    }
+  }, [tokensSold]);
+
+  React.useEffect(() => {
+    if (currentEthBalance) {
+      const converted_value = Number(currentEthBalance["value"]) / 1000000000000000000
+      setEthToUseInBuyFloat(converted_value);
+      setEthToUseInBuyWei(currentEthBalance["value"])
+      setCurrentETHBalanceState(BigInt(toWei(currentEthBalance["value"], "wei")));
+    }
+  }, [currentEthBalance]);
+
+  // TODO: make tier dynamic?
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEthToUseInBuyFloat(parseFloat(event.target.value));
+    const ethInWei = BigInt(Number(event.target.value) * 1000000000000000000)
+    setEthToUseInBuyWei(ethInWei)
+    // setTokensThatCanBeBoughtWithCurrentEthState(BigInt(tokenAmount))
+  };
+
+  return (
+    
+    <div className="page" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '30px' }}>
+     <div className='prophettop'>
+      <Typography className="container" variant="h2">$PROPHET Zone</Typography>
+      <div className='container'>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className={styles.table}>$PROPHET tokens owned by 0x{String(address).slice(-4)}:</TableCell>
+                <TableCell className={styles.table}>{Math.floor(Number(toWei(Number(currentTokenBalanceState), "wei")) / 1000000000000000000)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>Total $PROPHET tokens staked:</TableCell>
+                <TableCell className={styles.table}>{Number(totalStakedTokensState)}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>$PROPHET staked by 0x{String(address).slice(-4)}:</TableCell>
+                <TableCell className={styles.table}>{Number(userStakedTokensState)}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div className="container">
+        <Button className={styles.button}
+          style={{ marginTop: 24, marginLeft: 15 }}
+          disabled={!buy || isBuyLoading || isBuyStarted}
+          data-mint-started={isBuyLoading && !isBuyStarted}
+          data-mint-complete={!isBuyLoading && isBuyStarted}
+          onClick={() =>
+            buy?.({
+              ...buyTokensContractConfig,
+            })
+          }
+        >
+          {!isBuyLoading && !isBuyStarted && "BUY"}
+          {isBuyLoading && 'Executing...'}
+          {!isBuyLoading && isBuyStarted && "complete"}
+        </Button>
+        <TextField className={styles.textbox}
+          label="ETH Provided"
+          type="number"
+          value={ethToUseInBuyFloat}
+          onChange={handleChange}
+          InputLabelProps={{
+            style: {color: 'violet', transform: 'translateY(-20px)'  }
+          }}
+          InputProps={{
+            style: {color: "black"}
+          }}  
+          style={{ marginTop: 24, marginLeft: 15 }}
+        />
+      </div>
+      <div className='container'>
+        <Typography>will result in approx. {Math.floor(Number(toWei(Number(tokensThatCanBeBoughtWithCurrentEthState), "wei")) / 1000000000000000000)} $PROPHET tokens!</Typography>
+      </div>
+      <div className='container'>
+        <Button className={styles.button}
+          style={{ marginTop: 24, marginLeft: 15 }}
+          disabled={!buy || isBuyLoading || isBuyStarted}
+          onClick={() => {
+            setEthToUseInBuyFloat(0.1)
+            setEthToUseInBuyWei(BigInt(0.1 * 1000000000000000000))
+          }
+          }
+        >
+          .1
+        </Button>
+        <Button className={styles.button}
+          style={{ marginTop: 24, marginLeft: 15 }}
+          disabled={!buy || isBuyLoading || isBuyStarted}
+          onClick={() => {
+            setEthToUseInBuyFloat(0.5)
+            setEthToUseInBuyWei(BigInt(0.5 * 1000000000000000000))
+          }
+          }
+        >
+          .5
+        </Button>
+        <Button className={styles.button}
+          style={{ marginTop: 24, marginLeft: 15 }}
+          disabled={!buy || isBuyLoading || isBuyStarted}
+          onClick={() => {
+            setEthToUseInBuyFloat(1)
+            setEthToUseInBuyWei(BigInt(1 * 1000000000000000000))
+          }
+          }
+        >
+          1
+        </Button>
+        <Button className={styles.button}
+          style={{ marginTop: 24, marginLeft: 15 }}
+          disabled={!buy || isBuyLoading || isBuyStarted}
+          onClick={() => {
+            setEthToUseInBuyFloat(Number(currentETHBalanceState) / 1000000000000000000)
+            setEthToUseInBuyWei(BigInt(Number(currentETHBalanceState)))
+          }
+          }
+        >
+          MAX
+        </Button>
+      </div>
+     </div>
+      <div className="container" style={{ marginTop: "20px" }}>
+        <Typography variant="h3">Staking</Typography>
+      </div>
+      <div>
+        <ProphetApproveAndStakeCard mounted={mounted} isConnected={isConnected} cardTitle='Stake $PROPHET' currentTokenBalanceState={currentTokenBalanceState} setCurrentTokenBalanceState={setCurrentTokenBalanceState}></ProphetApproveAndStakeCard>
+        <ProphetApproveAndUnstakeCard mounted={mounted} isConnected={isConnected} cardTitle='Unstake $PROPHET' currentTokenBalanceState={currentTokenBalanceState} setCurrentTokenBalanceState={setCurrentTokenBalanceState}></ProphetApproveAndUnstakeCard>
+      </div>
+      <div className="container" style={{ marginTop: "20px" }}>
+        <Typography variant="h3">Stages</Typography>
+      </div>
+      <div className="container">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className={styles.table}>ETH per token</TableCell>
+                <TableCell className={styles.table}>Tokens left</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell className={styles.table}>.000000003125</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[5])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[5]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>.000000006250</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[4])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[4]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>.000000012500</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[3])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[3]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>.000000025000</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[2])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[2]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>.000000050000</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[1])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[1]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className={styles.table}>.000000100000</TableCell>
+                <TableCell className={styles.table}>{String(tokensRemaining[0])}</TableCell>
+                <TableCell>
+                  <CircularProgress color="secondary"
+                    sx={{
+                      height: 25, // Adjust the height as needed
+                      width: '100%', // Make the bar wider
+                    }} variant="determinate" value={(Number(tokensRemaining[0]) / 20000000000) * 100} />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Prophet;
