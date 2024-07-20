3246d2dba7d2c5b28884f722067705a1479b5842
spergcity <60526075+spergcity@users.noreply.github.com>
2024-07-20 14:41:40 -0700

    Update ProphetApproveAndUnstakeCard.tsx

--- components/approval_cards/ProphetApproveAndUnstakeCard.tsx
+++ components/approval_cards/ProphetApproveAndUnstakeCard.tsx
@@ -155,7 +155,7 @@ const ProphetApproveAndUnstakeCard: React.FC<ProphetApproveAndUnstakeCardProps> 
                 <ErrorAlert errorMessage={errorMessage} setErrorMessage={setErrorMessage}></ErrorAlert>
                 <div style={{ padding: '24px 24px 24px 0' }}>
                     <Typography variant="h5">{cardTitle}</Typography>
-                    <TextField
+                    <TextField className={styles.textbox}
                         label="$PROPHET Amount (ETHER)"
                         type="number"
                         value={Number(tokenAmountToRemove)}
