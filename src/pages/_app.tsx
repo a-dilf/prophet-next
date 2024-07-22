c18ecbb0564bdab8c0e356789be2a39eaaaecd0c
bunsterwunster <maxduck1@yahoo.com>
2024-07-21 20:58:25 -0700

    Set dark theme for connect button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,18 @@
-import '../styles/global.css';
+import "../styles/global.css";
 
-import '@rainbow-me/rainbowkit/styles.css';
-import React from "react";
-import type { AppProps } from 'next/app';
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
 
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../styles/theme'; // Adjust the path according to your file structure
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../styles/theme"; // Adjust the path according to your file structure
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { midnightTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
 
-import { config } from '../wagmi';
+import { config } from "../wagmi";
 
-import Layout from '../../components/Layout';
+import Layout from "../../components/Layout";
 
 const queryClient = new QueryClient();
 
@@ -21,10 +20,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        <RainbowKitProvider
+          theme={midnightTheme({ ...midnightTheme.accentColors.pink, borderRadius: "small", fontStack: "system" })}
+        >
           <ThemeProvider theme={theme}>
             <Layout>
-              <Component {...pageProps} /> 
+              <Component {...pageProps} />
             </Layout>
           </ThemeProvider>
         </RainbowKitProvider>
