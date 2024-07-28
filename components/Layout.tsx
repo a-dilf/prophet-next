bab402e67ce6a1e00f6c1b0690e4a34d8d9fc4d7
bunsterwunster <maxduck1@yahoo.com>
2024-07-27 19:11:54 -0700

    refactor: Update Layout component with improved NavBar and main styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,21 @@
-import React, { ReactNode, useState } from "react";
-import NavBar from "./NavBar";
+// components/Layout.js
+import React, { ReactNode } from 'react';
+import NavBar from './NavBar';
 
 interface LayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  return (
-    <div className="min-h-screen bg-black text-violet-300">
-      <NavBar open={sidebarOpen} setOpen={setSidebarOpen} />
-      <main className="pt-16 pb-24 px-4 sm:px-6 lg:px-8">
-        {children}
-      </main>
-    </div>
-  );
+    const [open, setOpen] = React.useState(false); // State management for NavBar
+
+    return (
+        <>
+            <NavBar open={open} setOpen={setOpen} />
+            <main style={{ marginBottom: "100px", marginTop: "100px" }} >{children}</main>
+        </>
+    );
 };
 
 export default Layout;
