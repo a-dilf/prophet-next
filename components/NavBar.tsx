ac28d2327ac46aceba725e6cb5310f5e2762bc5e
a-dilf <161424799+a-dilf@users.noreply.github.com>
2024-09-07 08:44:00 -0700

    update discord link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -64,7 +64,7 @@ const Navbar: React.FC<NavbarProps> = ({ open, setOpen }) => {
                 {/* Add this Box component */}
                 <div>
                   <a
-                    href="https://discord.gg/dRYtVaS43A"
+                    href="https://discord.gg/dCPnBxxF"
                     target="_blank"
                     rel="noopener noreferrer"
                     style={{ color: "white" }}
