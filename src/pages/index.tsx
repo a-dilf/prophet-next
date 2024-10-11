78860f71c27a077491d3e266b2c71da4b5b7f38d
a-dilf <dilf.dev@gmail.com>
2024-10-11 16:53:42 -0700

    adding cigs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -137,6 +137,18 @@ const Home: NextPage = () => {
     { src: 'white_hearts/2450.avif', alt: 'Description 1' },
   ];
 
+  const cigarette_images = [
+    { src: 'cigarette/1211.avif', alt: 'Description 1' },
+    { src: 'cigarette/2602.png', alt: 'Description 1' },
+    { src: 'cigarette/2604.avif', alt: 'Description 1' },
+    { src: 'cigarette/3177.avif', alt: 'Description 1' },
+    { src: 'cigarette/3774.avif', alt: 'Description 1' },
+    { src: 'cigarette/7439.avif', alt: 'Description 1' },
+    { src: 'cigarette/7546.avif', alt: 'Description 1' },
+    { src: 'cigarette/8950.avif', alt: 'Description 1' },
+    { src: 'cigarette/8951.avif', alt: 'Description 1' },
+  ];
+
   return (
     <div className="page" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '35px' }}>
       <div className="homepagetop">
@@ -264,22 +276,24 @@ const Home: NextPage = () => {
         <Typography className="container" variant="h4">The Sweep Vault</Typography>
         <Typography sx={{ paddingTop: "15px", paddingBottom: "15px" }}>Prophet Lady operates as a Milady derivative and complies with the mandate to sweep Remco assets--including related ecosystem NFTs. Sweeped assets can be seen below.</Typography>
 
-        <Accordion key={54312}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={`54312`}
-            id={`54312`}
-          >
-            <Typography>Sweep Vault Wallet</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Box component={Link} href={"https://opensea.io/0x58D37267e48a3A4678e2a6e99e52b74a576c0240"} target="_blank" rel="noopener noreferrer" underline="none">
-              <ListItem >
-                <ListItemText primary={"0x" + "0x58D37267e48a3A4678e2a6e99e52b74a576c0240".slice(0, 4) + "..." + "0x58D37267e48a3A4678e2a6e99e52b74a576c0240".slice(-4)} secondary="👉 Open Sea Link" />
-              </ListItem>
-            </Box>
-          </AccordionDetails>
-        </Accordion>
+        <div className="container">
+          <Accordion key={54312}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`54312`}
+              id={`54312`}
+            >
+              <Typography>Sweep Vault Wallet</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Box component={Link} href={"https://opensea.io/0x58D37267e48a3A4678e2a6e99e52b74a576c0240"} target="_blank" rel="noopener noreferrer" underline="none">
+                <ListItem >
+                  <ListItemText primary={"0x" + "0x58D37267e48a3A4678e2a6e99e52b74a576c0240".slice(0, 4) + "..." + "0x58D37267e48a3A4678e2a6e99e52b74a576c0240".slice(-4)} secondary="👉 Open Sea Link" />
+                </ListItem>
+              </Box>
+            </AccordionDetails>
+          </Accordion>
+        </div>
 
         <Typography variant="h5" component="div" sx={{ mb: 2, paddingTop: "15px" }}> {/* Customize the title here */}
           Oekaki Maker
@@ -290,6 +304,11 @@ const Home: NextPage = () => {
           White Hearts
         </Typography>
         <ImageBar images={white_heart_images}></ImageBar>
+
+        <Typography variant="h5" component="div" sx={{ mb: 2, paddingTop: "15px" }}> {/* Customize the title here */}
+          Cigarettes
+        </Typography>
+        <ImageBar images={cigarette_images}></ImageBar>
       </div>
 
       <div className="container">
