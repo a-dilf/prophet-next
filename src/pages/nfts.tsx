b68ef50b4cb23cccaa482728cf2f4dced9afcf01
a-dilf <161424799+a-dilf@users.noreply.github.com>
2024-12-18 11:09:04 -0800

    Removed temp table

--- src/pages/nfts.tsx
+++ src/pages/nfts.tsx
@@ -309,30 +309,6 @@ const Nfts: NextPage = () => {
                 </Button>
             </div>
             <div className='container'>
-                    <TableContainer>
-                        <Table>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell className={styles.table}>ID: X</TableCell>
-                                    <TableCell className={styles.table}>tier</TableCell>
-                                    <TableCell className={styles.table}>level button</TableCell>
-                                    <TableCell className={styles.table}>level max button</TableCell>
-                                    <TableCell className={styles.table}>stake button</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell className={styles.table}>ID: X</TableCell>
-                                    <TableCell className={styles.table}>tier</TableCell>
-                                    <TableCell className={styles.table}>level button</TableCell>
-                                    <TableCell className={styles.table}>level max button</TableCell>
-                                    <TableCell className={styles.table}>stake button</TableCell>
-                                    <TableCell className={styles.table}>opensea link to image</TableCell>
-                                </TableRow>
-                                
-                                
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </div>
             {displayedCards.map((cardData, index) => (
                 <NftCard
                     key={index}
@@ -343,6 +319,7 @@ const Nfts: NextPage = () => {
                     setStateAllowanceAmount={cardData.setStateAllowanceAmount}
                 />
             ))}
+            </div>
             <div>
                 <Button
                     color="secondary"
