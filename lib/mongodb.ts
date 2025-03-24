4bf65ed2a02aa8e3ebd408ceb5cfdbc17da0deca
a-dilf <dilf.dev@gmail.com>
2025-03-23 21:39:58 -0800

    another test

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string
+const uri = "mongodb+srv://dilfdev:guvuUxssx01gfypQ@cluster0.alsc6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true"
 const options = { appName: "devrel.template.nextjs" };
 
 let client: MongoClient;
