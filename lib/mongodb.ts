7224b35973f7ba248708671853e367a2ee648bac
a-dilf <dilf.dev@gmail.com>
2025-03-23 21:23:12 -0800

    format fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,38 +1,27 @@
 import { MongoClient } from "mongodb";
 
-// Get connection string from environment variables
 const uri = process.env.MONGODB_URI as string
-if (!uri) {
-  throw new Error("Please define the MONGODB_URI environment variable");
-}
-
-const options = {
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-};
+const options = { appName: "devrel.template.nextjs" };
 
 let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
+    _mongoClient?: MongoClient;
   };
 
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
   }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  client = globalWithMongo._mongoClient;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
+// Export a module-scoped MongoClient. By doing this in a
 // separate module, the client can be shared across functions.
-export default clientPromise;
+
+export default client;
