0f8e34d54b3e2c7abbadb90f5ff6c4ff241c5c73
a-dilf <dilf.dev@gmail.com>
2025-11-14 00:00:45 -0800

    adding support for new bucket domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['bafybeid6ya7l4yiwdaslup2hm47z3x4rqjh5h3ulthtxkjfsgdbpq2qsl4.ipfs.nftstorage.link'],
+    domains: ['prophet-lady.s3.amazonaws.com'],
   },
 };
 
