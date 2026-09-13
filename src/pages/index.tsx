3296d593470c529054114aa1ee333dfb12d88885
spergcity <60526075+spergcity@users.noreply.github.com>
2026-09-13 15:03:30 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -532,7 +532,7 @@ const Home: NextPage = () => {
             <ListItemIcon>
             <CircleIcon fontSize="small" sx={{ color: "#ee82ee", opacity: 100 }} />
             </ListItemIcon>
-            <ListItemText primary="Total $ spent on buybacks: >$28,900" />
+            <ListItemText primary="Total $ spent on buybacks: >$30,400" />
           </ListItem>
         </List>
       </div>
